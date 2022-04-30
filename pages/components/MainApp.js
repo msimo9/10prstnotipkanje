f3353942e37548bbcd25bc4a0497a903272e26c5
msimo9 <matjaz.simonic@os-ajdovscina.si>
2022-04-30 16:10:21 +0200

    added all the lessons

--- pages/components/MainApp.js
+++ pages/components/MainApp.js
@@ -1,16 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import useEventListener from '@use-it/event-listener'
 import allWords from './slovenianWords';
-import { lesson1, lesson2 } from './words';
+import { lessons } from './words';
 
 const MainApp = ({setGameStarted, numberOfWords, difficulty}) => {
   const [words, setWords] = useState([]);
   const [wordsReady, setWordsReady] = useState(false);
+  const [typedCharacters, setTypedCharacters] = useState([]);
+  const [incorrect, setIncorrect] = useState(false);
   const [screenH, setScreenH] = useState(1920);
   const [screenW, setScreenW] = useState(1080);
   const [speed, setSpeed] = useState(difficulty === 1 ? 30 : difficulty === 2 ? 20 : 10);
   const [errors, setErrors] = useState(0);
+  const [charErrors, setCharErrors] = useState(0);
+
   const generateWords = (arr) => {
+    setErrors(0);
+    setCharErrors(0);
+    setWords([]);
     const width  = window.innerWidth || document.documentElement.clientWidth || 
     document.body.clientWidth;
     const height = window.innerHeight|| document.documentElement.clientHeight|| 
@@ -18,51 +25,79 @@ const MainApp = ({setGameStarted, numberOfWords, difficulty}) => {
     setScreenH(height);
     setScreenW(width);
     let tempArr = [];
-    for(let i = 0; i<numberOfWords; i++){
-      tempArr.push(
-        {
-          word: arr[Math.floor(Math.random()*arr.length-1)],
-          posX: Math.floor(Math.random()*(width*0.7)+1),
-          posY: 0}
-      );
+    for(let i = 0; i < arr.length ; i++){
+      for(let j = 0; j<numberOfWords; j++){
+        tempArr.push(
+          {
+            word: arr[i][Math.floor(Math.random()*arr[i].length-1)],
+            posX: Math.floor(Math.random()*(width*0.7)+1),
+            posY: 0}
+        );
+      }
     }
     
-    console.log("tempArr: ", tempArr)
+    
     setWords(tempArr);
     setWordsReady(true);
   }
 
   const removeWord = () => {
+    setTypedCharacters([]);
     let tempArr = words;
     tempArr.shift();
     setWords(tempArr);
-    console.log("words: ",words);
-    console.log("wordsL: ", words.length);
     if(words.length === 0){
       setWordsReady(false);
-      generateWords(lesson2);
-      //setGameStarted(false);
-      //const alertString = "bravo, šlo ti je dobro, imel si " +  errors + " napak.";
-      //alert(alertString)
+      setGameStarted(false);
+      let errorWord = "";
+      if(charErrors%10 === 1){
+        errorWord = " napako."
+      }
+      else if(charErrors%10 === 2){
+        errorWord = " napaki."
+      }
+      else if(charErrors%10 === 3 || charErrors%10 === 4){
+        errorWord = " napake."
+      }
+      else{
+        errorWord = " napak."
+      }
+      const alertString = "bravo, šlo ti je dobro, imel si " +  charErrors + errorWord;
+      alert(alertString)
     };
   }
 
   function handler({ key }) {
     if(wordsReady){
-      if (key === words[0].word.charAt(0) || words[0].word.charAt(0) === "⎵" && key === " ") {
+      if (key.toUpperCase() === words[0].word.charAt(0).toUpperCase() || words[0].word.charAt(0).toUpperCase() === "⎵" && key.toUpperCase() === " ") {
         let tempArr = words;
+
+        if(!incorrect){
+          let tempObj = {char: words[0].word.charAt(0), correct: true}
+          setTypedCharacters(typedCharacters => [...typedCharacters, tempObj])
+        }else{
+          let tempObj = {char: words[0].word.charAt(0), correct: false}
+          setTypedCharacters(typedCharacters => [...typedCharacters, tempObj])
+          setIncorrect(false);
+        }
+        
+
+
         tempArr[0].word = tempArr[0].word.substring(1);
         if(tempArr[0].word === ""){
           removeWord();
         };
         setWords(tempArr);
+      }else{
+        setCharErrors(charErrors+1);
+        setIncorrect(true);
       }
     }
   }
   useEventListener('keydown', handler);
 
   useEffect(() => {
-    generateWords(lesson1);
+    generateWords(lessons);
   }, [])
 
   
@@ -71,7 +106,6 @@ const MainApp = ({setGameStarted, numberOfWords, difficulty}) => {
   useEffect(() => {
       if(wordsReady && words[0]){
       const intervalId = setInterval(() => {
-        if(words.lengt <= 1) console.log(words[0]);
         let tempArr = words;
         tempArr[0].posY += 1;
         if(tempArr[0].posY > (screenH*0.8-40)){
@@ -93,11 +127,23 @@ const MainApp = ({setGameStarted, numberOfWords, difficulty}) => {
   return (
     wordsReady && words.length > 0 ?
     <div className='w-4/5 h-4/5 border-4 border-white shadow-2xl relative flex justify-center items-center'>
-      <div className='fixed top-0'>ZGREŠENE BESEDE: {errors}</div>
+      <div className='w-full h-auto flex-col flex justify-center items-center space-y-2 fixed top-0 mt-2'>
+        <div className=''>ZGREŠENE BESEDE: {errors}</div>
+        <div className=''>ZGREŠENE ČRKE: {charErrors}</div>
+      </div>
       {
         words[0].word !== undefined && 
         <div className={`absolute bg-white shadow-2xl rounded-sm w-auto px-4 h-10 flex justify-center items-center `} style={{top: words[0].posY, left: words[0].posX}} >
-          <span className='text-black tracking-wide'>{words[0].word ? words[0].word.toUpperCase() : ""}</span>
+          <span className='text-black tracking-wide'>
+            {typedCharacters.length > 0 ? typedCharacters.map((item, index) => {
+              return(
+                item.correct ?
+                  <span className='text-green-500' key={index}>{typedCharacters[index].char.toUpperCase()}</span>
+                : <span className='text-red-500' key={index}>{typedCharacters[index].char.toUpperCase()}</span>
+              )
+            }) : ""}
+            {words[0].word ? words[0].word.toUpperCase() : ""}
+          </span>
         </div>
       }
     </div>
